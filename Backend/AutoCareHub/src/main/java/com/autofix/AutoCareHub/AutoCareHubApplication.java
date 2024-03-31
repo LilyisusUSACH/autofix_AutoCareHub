f06c5e0e7452b77dfd6e546fe3fe0f001bcf42e6
Alejandro J. Cortes <alejandro.cortes.c@usach.cl>
2024-03-31 10:11:27 -0300

    [Alejandro Cortes] General - Creacion del proyecto y Backend

---
+++ Backend/AutoCareHub/src/main/java/com/autofix/AutoCareHub/AutoCareHubApplication.java
@@ -0,0 +1,13 @@
+package com.autofix.AutoCareHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCareHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCareHubApplication.class, args);
+	}
+
+}
